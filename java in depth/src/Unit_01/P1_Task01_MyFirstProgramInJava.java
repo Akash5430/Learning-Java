3bb464d0340c8b67134a3653eba9e2f209a2f789
Acer <Acer@LAPTOP-IF4T4HN2>
2022-04-10 13:39:16 +0000

    First Commit

---
+++ java in depth/src/Unit_01/P1_Task01_MyFirstProgramInJava.java
@@ -0,0 +1,11 @@
+package Unit_01;
+
+public class P1_Task01_MyFirstProgramInJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("HEllo WOrld...!\n");
+		System.out.println("This IS My First Java Program..:)");
+	}
+
+}
